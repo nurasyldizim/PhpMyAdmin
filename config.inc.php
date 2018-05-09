504ee3648395f997d9964dc33b64bbedcba29222
nurasyldizim <nurasyl.dizim@gmail.com>
2018-05-10 01:13:29 +0600

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'qgmnmu-mysql';
+$cfg['Servers'][$i]['host'] = 'nmuaircontrol-mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
