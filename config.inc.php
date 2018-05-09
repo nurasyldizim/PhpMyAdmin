3876d1f0763b77bb983a684e7bc11f165febda6c
nurasyldizim <nurasyl.dizim@gmail.com>
2018-05-10 02:13:08 +0600

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,9 +28,9 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'qgmnmu-mysql';
+$cfg['Servers'][$i]['host'] = 'iituqazgidromet-mysql';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /**
  * phpMyAdmin configuration storage settings.
